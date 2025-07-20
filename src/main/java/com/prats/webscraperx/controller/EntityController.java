2ea9c86d1bd87a704f74a0188ddd78c85fca9c1a
Prathmesh Dubey <prathmeshdubey10121973@gmail.com>
2025-07-21 01:10:04 +0530

    feat(controller): complete PatternController and EntityController

--- src/main/java/com/prats/webscraperx/controller/EntityController.java
+++ src/main/java/com/prats/webscraperx/controller/EntityController.java
@@ -1,21 +1,75 @@
 package com.prats.webscraperx.controller;
 
+import com.prats.webscraperx.model.Entity;
 import com.prats.webscraperx.service.EntityService;
 import com.prats.webscraperx.service.PatternService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/webscraperx/api")
 public class EntityController {
 
     @Autowired
-    private EntityService EntityService;
+    private EntityService entityService;
 
     @Autowired
     private PatternService patternService;
 
+    @GetMapping("/getEntity/all")
+    public List<Entity> getAllEntities() {
+        return entityService.getList();
+    }
 
+    @GetMapping("/getEntity/{id}")
+    public Entity getEntityById(Long id) {
+        return entityService.getEntityById(id).orElse(null);
+    }
 
+    @GetMapping("/getEntity/byUrl")
+    public List<Entity> getByUrl(String url) {
+        return entityService.getByUrl(url);
+    }
+
+//    @PostMapping("/scrapeByPattern")
+//    public List<Entity> scrapeByPattern(String url, String pattern) {
+//    }
+
+//    @PostMapping("/scrapeByNewPattern")
+//    public String scrapeByNewPattern(String url, String pattern) {
+//    }
+
+    @DeleteMapping("/deleteEntity/{id}")
+    public String deleteEntity(@PathVariable("id") Long id) {
+        entityService.delete(id);
+        return "Entity with ID " + id + " deleted successfully.";
+    }
+
+    @DeleteMapping("/deleteEntity/all")
+    public String deleteAllEntities() {
+        entityService.deleteAll();
+        return "All entities deleted successfully.";
+    }
+
+    @PostMapping("/downloadAttachment/{id}")
+    public String downloadAttachment(@PathVariable("id") Long id, @RequestBody String fileName, @RequestBody String downloadPath) throws IOException {
+        Entity entity = entityService.getEntityById(id).orElse(null);
+        if (entity != null) {
+            return entityService.downloadAttachment(id, fileName, downloadPath);
+        }
+        return "Entity not found.";
+    }
+
+    @GetMapping("/showAttachment/{id}")
+    public void showAttachment(@PathVariable("id") Long id, @RequestBody String fileName) throws IOException {
+        Entity entity = entityService.getEntityById(id).orElse(null);
+        if (entity != null) {
+            entityService.showAttachment(id, fileName);
+        } else {
+            throw new RuntimeException("Entity not found.");
+        }
+    }
 }
---
+++ src/main/java/com/prats/webscraperx/controller/PatternController.java
@@ -0,0 +1,63 @@
+package com.prats.webscraperx.controller;
+
+import com.prats.webscraperx.model.Pattern;
+import com.prats.webscraperx.service.PatternService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/webscraperx/api/pattern")
+public class PatternController {
+
+    @Autowired
+    private PatternService patternService;
+
+    @GetMapping("/getAllPatterns")
+    public List<Pattern> getAllPatterns(){
+        return patternService.getAllPatterns();
+    }
+
+    @GetMapping("/getPattern/{id}")
+    public Pattern getPatternById(@PathVariable("id") Long id) {
+        return patternService.getPatternById(id).orElse(null);
+    }
+
+    @PostMapping("/createPattern")
+    public String createPattern(@RequestBody Pattern pattern) {
+        if (patternService.patternExists(pattern.getPatternName())) {
+            return "Pattern already exists";
+        } else {
+            patternService.createPattern(pattern);
+            return "Pattern succesfully created.";
+        }
+    }
+
+    @PutMapping("/updatePattern/{id")
+    public String updatePattern(@PathVariable("id") Long id, @RequestBody Pattern pattern) {
+        if (patternService.patternExists(id)) {
+            pattern.setId(id);
+            patternService.updatePattern(pattern);
+            return "Pattern successfully updated.";
+        } else {
+            return "Pattern not found.";
+        }
+    }
+
+    @DeleteMapping("/deletePattern/{id}")
+    public String deletePattern(@PathVariable("id") Long id) {
+        if (patternService.patternExists(id)) {
+            patternService.deletePattern(id);
+            return "Pattern successfully deleted.";
+        } else {
+            return "Pattern not found.";
+        }
+    }
+
+    @DeleteMapping("/deleteAllPatterns")
+    public String deleteAllPatterns() {
+        patternService.deleteAllPatterns();
+        return "All patterns successfully deleted.";
+    }
+}
