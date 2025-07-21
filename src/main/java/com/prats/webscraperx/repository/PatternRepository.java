7a3a51723a5762cdbb4e58bba1394b5be8b9a8dd
Prathmesh Dubey <prathmeshdubey10121973@gmail.com>
2025-07-21 18:55:33 +0530

    feat(repository): add PatternRepository interface

--- src/main/java/com/prats/webscraperx/repository/PatternRepository.java
+++ src/main/java/com/prats/webscraperx/repository/PatternRepository.java
@@ -1,12 +1,8 @@
 package com.prats.webscraperx.repository;
 
 import com.prats.webscraperx.model.Pattern;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatternRepository extends JpaRepository<Pattern, Long> {
 
-    // Define custom query methods if needed
-    // For example, to find patterns by a specific attribute:
-    // List<Pattern> findByAttribute(String attribute);
-
-    // You can also use @Query annotations for more complex queries
 }
