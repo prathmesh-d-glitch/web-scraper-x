0cbe6c5ea2963dc36fef85423a73e842379e91ea
Prathmesh Dubey <prathmeshdubey10121973@gmail.com>
2025-07-26 16:27:30 +0530

    refactor(config): enhance ChromeOptions in ScraperConfig to reduce bot detection

--- src/main/java/com/prats/webscraperx/config/ScraperConfig.java
+++ src/main/java/com/prats/webscraperx/config/ScraperConfig.java
@@ -11,7 +11,15 @@ public class ScraperConfig {
     public WebDriver setUpWebDriver(boolean headlessMode) {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);  // Use EAGER to load the page as quickly as possible without waiting for all resources to load
+
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--disable-gpu");
+
+        //Helps to avoid basic bot detection mechanisms
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+        options.setExperimentalOption("useAutomationExtension", false);
 
         if(headlessMode) {
             options.addArguments("--headless=new");
